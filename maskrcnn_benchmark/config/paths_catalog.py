a8b2d6edab2061b1173d054839ecaf55e2a15c2d
Andrei Amatuni <andrei.amatuni@gmail.com>
2020-03-03 12:34:58 -0500

    fix toyroom dataloader

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -284,6 +284,17 @@ class DatasetCatalog(object):
                 factory="HOMEDataset",
                 args=args,
             )
+        elif "toyroom" in name:
+            data_dir = DatasetCatalog.DATA_DIR
+            attrs = DatasetCatalog.DATASETS[name]
+            args = dict(
+                root=os.path.join(data_dir, attrs["img_dir"]),
+                ann_file=os.path.join(data_dir, attrs["ann_file"]),
+            )
+            return dict(
+                factory="ToyroomDataset",
+                args=args,
+            )
         elif "cityscapes" in name:
             data_dir = DatasetCatalog.DATA_DIR
             attrs = deepcopy(DatasetCatalog.DATASETS[name])
